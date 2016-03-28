888a513722d68864ed7145358d00de498a02be87
shadowfan10001 <kstickler@chicagotechacademy.org>
2016-03-28 18:51:17 +0000

    added comment

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
-  
+  # stuff
   def index
     @articles = Article.all
   end
